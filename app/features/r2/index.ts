171c46d546e81bdede42dcfbbee6016644843c51
sofuetakuma <kawahagi0620@gmail.com>
2024-05-12 19:36:28 +0900

    不要なコードを削除

--- app/features/r2/index.ts
+++ app/features/r2/index.ts
@@ -1,22 +1,6 @@
 import { colors } from "@/lib/console";
-import { AppLoadContext, json } from "@remix-run/cloudflare";
+import { AppLoadContext } from "@remix-run/cloudflare";
 import { v4 as uuidv4 } from "uuid";
-// import {
-//   PutObjectCommand,
-//   GetObjectCommand,
-//   S3Client,
-// } from "@aws-sdk/client-s3";
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
-// const createS3Client = (context: AppLoadContext) =>
-//   new S3Client({
-//     endpoint: context.cloudflare.env.R2_ENDPOINT_URL,
-//     region: "auto",
-//     credentials: {
-//       accessKeyId: context.cloudflare.env.R2_ACCESS_KEY_ID,
-//       secretAccessKey: context.cloudflare.env.R2_SECRET_ACCESS_KEY,
-//     },
-//   });
 
 type S3ImageType = "avatars" | "posts";
 
@@ -29,21 +13,6 @@ async function uploadImageToS3(
   const key = `${type}/${uuidv4()}`;
 
   const arrayBuffer = await file.arrayBuffer();
-  // const body = new Uint8Array(arrayBuffer);
-
-  // const params = {
-  //   Bucket: context.cloudflare.env.BUCKET_NAME,
-  //   Key: key,
-  //   Body: body,
-  // };
-  // const command = new PutObjectCommand(params);
-
-  // try {
-  //   const client = createS3Client(context);
-  //   await client.send(command);
-  // } catch (err) {
-  //   console.error(err);
-  // }
 
   await context.cloudflare.env.R2.put(key, arrayBuffer, {
     httpMetadata: {
@@ -62,21 +31,6 @@ async function getImageUrlFromS3(context: AppLoadContext, key: string | null) {
   if (key.startsWith("http") || key.startsWith("https")) {
     return key;
   }
-
-  // const client = createS3Client(context);
-  // const params = {
-  //   Bucket: context.cloudflare.env.BUCKET_NAME,
-  //   Key: key,
-  // };
-  // const command = new GetObjectCommand(params);
-  // const url = await getSignedUrl(client, command, { expiresIn: 3600 });
-  // return url;
-
-  // const object = await context.cloudflare.env.R2.get(key);
-
-  // if (object === null) {
-  //   throw json({ message: "Object not found" }, { status: 404 });
-  // }
 
   return `/images/${key}`
 }
