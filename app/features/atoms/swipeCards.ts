c76e04cf4049e5484b023a153bcf15ff68cb329c
sofuetakuma <kawahagi0620@gmail.com>
2024-05-12 09:54:51 +0900

    未使用のコードを削除

--- app/features/atoms/swipeCards.ts
+++ app/features/atoms/swipeCards.ts
@@ -1,6 +1,4 @@
 import { atom } from 'jotai';
 
-export const recommendCurrentIndexAtom = atom(0);
 export const recommendCurrentScrollIndexAtom = atom(0);
-export const followingCurrentIndexAtom = atom(0);
 export const followingCurrentScrollIndexAtom = atom(0);
--- app/features/types/models/user.ts
+++
@@ -1,6 +0,0 @@
-export type User = {
-  id: string;
-  name: string | null;
-  icon: string | null;
-  createdAt: string;
-};
